98ec890982cb086554f1f61decf505205eaae69a
Waleed Fasihi <waleed3594@gmail.com>
2022-11-16 22:38:50 +0500

    Add Replay Functionality

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 from board import Board
 
-board = Board()
-
 pieces = ["X", "O"]
 
-next_turn = True
-while next_turn:
-    for piece in pieces:
-        # Display the Grid
-        board.display()
 
-        # Show which player moves it is currently
-        print(f"Player {piece} - Move")
+# Function to Start a New Game
+def start_game():
+    board = Board()
 
-        piece_failed = True
-        while piece_failed:
-            # Player Moves making sure input is valid
-            row = board.get_input("row")
-            column = board.get_input("column")
+    next_turn = True
+    while next_turn:
+        for piece in pieces:
+            # Display the Grid
+            board.display()
 
-            # Place Piece on the Grid and Check final move
-            piece_failed = board.place_piece(piece, row, column)
+            # Show which player moves it is currently
+            print(f"Player {piece} - Move")
 
-        # Check if there is a winner or draw
-        if board.is_winner(piece, row, column):
-            next_turn = False
-            break
+            piece_failed = True
+            while piece_failed:
+                # Player Moves making sure input is valid
+                row = board.get_input("row")
+                column = board.get_input("column")
+
+                # Place Piece on the Grid and Check final move
+                piece_failed = board.place_piece(piece, row, column)
+
+            # Check if there is a winner or draw
+            if board.is_winner(piece, row, column):
+                next_turn = False
+                break
+
+
+# Ask player to restart or end game
+is_playing = True
+while is_playing:
+    start_game()
+
+    if input("Enter 'y' to Restart, 'n' to End: ").lower() != "y":
+        is_playing = False
